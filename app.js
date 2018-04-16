747679e2c1bc8b93d565ad233cb5e21219dab4b3
Victor D. Verdev <tenacidtea@gmail.com>
2018-04-16 05:50:31 -0500

    searchByName case insensitivity almost working

--- app.js
+++ app.js
@@ -1,147 +1,53 @@
-/*
-Build all of your functions for displaying and gathering information below (GUI).
-*/
+"use strict";
 
-// app is the function called to start the entire application
-function app(people){
-  var searchType = promptFor("Do you know the name of the person you are looking for? Enter 'yes' or 'no'", yesNo).toLowerCase();
-  switch(searchType){
-    case 'yes':
-    searchByName(people);
-    break;
-    case 'no':
-    searchByTraits(people);
-    break;
-    default:
-    alert("Wrong! Please try again, following the instructions dummy. :)");
-    app(people); // restart app
-    break;
-  }
+function runApp(arrayToBeSearched){
+	let searchType = getUserInput("Do you know the name of the person you are looking for? Enter 'yes' or 'no'", yesNoValidator).toLowerCase();
+	switch(searchType){
+		case 'yes':
+		searchByName(arrayToBeSearched);
+		break;
+	}
 }
 
-function searchByTraits(people) {
-  let userSearchChoice = prompt("What would you like to search by? 'height', 'weight', 'eye color', 'gender', 'age', 'occupation'.");
-  let filteredPeople;
-
-  switch(userSearchChoice) {
-    case "height":
-      filteredPeople = searchByHeight(people);
-      break;
-    case "weight":
-      filteredPeople = searchByWeight(people);
-      break;
-    // so on and so forth
-    default:
-      alert("You entered an invalid search type! Please try again.");
-      searchByTraits(people);
-      break;
-  }  
-
-  let foundPerson = filteredPeople[0];
-
-  mainMenu(foundPerson, people);
-
+function searchByName(arrayToBeSearched){
+	var firstNameVar = getUserInput("What is the person's first name?", chars);
+	var lastNameVar = getUserInput("What is the person's last name?", chars);
+	var searchByNameResults = [];
+	for (let index = 0; index < arrayToBeSearched.length; index++){
+		if (arrayToBeSearched[index].firstName.toLowerCase() == firstNameVar.toLowerCase() && arrayToBeSearched[index].lastName.toLowerCase() == lastNameVar.toLowerCase()){
+			searchByNameResults.push(arrayToBeSearched[index]);
+		}
+	}
+	displayPeople(searchByNameResults);
 }
 
-function searchByWeight(people) {
-  let userInputWeight = prompt("How much does the person weigh?");
-
-  let newArray = people.filter(function (el) {
-    if(el.weight == userInputWeight) {
-      return true;
-    }
-    // return true if el.height matches userInputHeight
-  });
-
-  return newArray;
+function displayPeople(arrayOfObjectsToBeSearched){
+	alert(arrayOfObjectsToBeSearched.map(function(personObject){
+		return personObject.firstName + " " + personObject.lastName;
+	}).join("\n"));
 }
 
-// Menu function to call once you find who you are looking for
-function mainMenu(person, people){
 
-  /* Here we pass in the entire person object that we found in our search, as well as the entire original dataset of people. We need people in order to find descendants and other information that the user may want. */
-
-  if(!person){
-    alert("Could not find that individual.");
-    return app(people); // restart
-  }
-
-  var displayOption = prompt("Found " + person.firstName + " " + person.lastName + " . Do you want to know their 'info', 'family', or 'descendants'? Type the option you want or 'restart' or 'quit'");
-
-  switch(displayOption){
-    case "info":
-    // TODO: get person's info
-    break;
-    case "family":
-    // TODO: get person's family
-    break;
-    case "descendants":
-    // TODO: get person's descendants
-    break;
-    case "restart":
-    app(people); // restart
-    break;
-    case "quit":
-    return; // stop execution
-    default:
-    return mainMenu(person, people); // ask again
-  }
+// Later investigate why this variable blows up when declared using "Let" //
+function getUserInput(promptMessage, inputValidator){
+	do{
+		var promptResponse = prompt(promptMessage).trim();
+	}
+	while(!promptResponse || !inputValidator(promptResponse));
+	return promptResponse;
 }
 
-function searchByName(people){
-  var firstN = promptFor("What is the person's first name?", chars);
-  var lastName = promptFor("What is the person's last name?", chars);
-  let results = [];
-    for (var i = 0; i < data.length; i++) {
-		alert(people[i].firstName.toLowerCase());
-		alert(firstN.toLowerCase);
-		alert(people[i].lastName.toLowerCase());
-		alert(lastName.toLowerCase);
-		alert(people[i].firstName.toLowerCase() == firstN.toLowerCase);
-		alert(people[i].lastName.toLowerCase() == lastName.toLowerCase);
-      if (people[i].firstName === firstN && people[i].lastName === lastName) {
-        results.push(people[i]);
-      }
-    }
-    displayPeople(results);
-    }
-
-  
-  
-  // TODO: find the person using the name they entered
-
-
-
-// alerts a list of people
-function displayPeople(people){
-  alert(people.map(function(person){
-    return person.firstName + " " + person.lastName;
-  }).join("\n"));
+function yesNoValidator(promptResponse){
+	return promptResponse.toLowerCase() === "yes" || promptResponse.toLowerCase() === "no";
 }
 
-function displayPerson(person){
-  // print all of the information about a person:
-  // height, weight, age, name, occupation, eye color.
-  var personInfo = "First Name: " + person.firstName + "\n";
-  personInfo += "Last Name: " + person.lastName + "\n";
-  // TODO: finish getting the rest of the information to display
-  alert(personInfo);
-}
-
-// function that prompts and validates user input
-function promptFor(question, valid){
-  do{
-    var response = prompt(question).trim();
-  } while(!response || !valid(response));
-  return response;
-}
-
-// helper function to pass into promptFor to validate yes/no answers
-function yesNo(input){
-  return input.toLowerCase() == "yes" || input.toLowerCase() == "no";
-}
-
-// helper function to pass in as default promptFor validation
 function chars(input){
-  return input; // default validation only
+	return input;
 }
+
+function getObjectPropertyValueCaseInsensitive(objectToBeSearched, valueToBeSearched){
+	return !!Object.values(objectToBeSearched).find(function(objectValue){
+		return objectValue.toLowerCase() === valueToBeSearched.toLowerCase();
+	})
+}
+
