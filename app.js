4c2f050a534d7294814463ea597d0b8acff1868a
Victor D. Verdev <tenacidtea@gmail.com>
2018-04-21 13:04:52 -0500

    Ready to abandon embedded if function

--- app.js
+++ app.js
@@ -1,28 +1,89 @@
 "use strict";
 
-function runApp(arrayToBeSearched){
+function runApp(arrayOfPeopleObjects){
 	let searchType = getUserInput("Do you know the name of the person you are looking for? Enter 'yes' or 'no'", yesNoValidator).toLowerCase();
 	switch(searchType){
 		case 'yes':
-		searchByName(arrayToBeSearched);
+		searchByName(arrayOfPeopleObjects);
+		break;
+		case 'no':
+		searchByTraits(arrayOfPeopleObjects);
+		break;
+		default:
+		alert("Wrong! Please try again, following the instructions dummy. :)");
+		runApp(arrayOfPeopleObjects); // restart app
 		break;
 	}
 }
 
-function searchByName(arrayToBeSearched){
+function searchByName(arrayOfPeopleObjects){
 	let firstNameVar = getUserInput("What is the person's first name?", chars);
 	let lastNameVar = getUserInput("What is the person's last name?", chars);
 	let searchByNameResults = [];
-	for (let index = 0; index < arrayToBeSearched.length; index++){
-		if (arrayToBeSearched[index].firstName.toLowerCase() === firstNameVar.toLowerCase() && arrayToBeSearched[index].lastName.toLowerCase() === lastNameVar.toLowerCase()){
-			searchByNameResults.push(arrayToBeSearched[index]);
+	for(let index = 0; index < arrayOfPeopleObjects.length; index++){
+		if (arrayOfPeopleObjects[index].firstName.toLowerCase() === firstNameVar.toLowerCase() && arrayOfPeopleObjects[index].lastName.toLowerCase() === lastNameVar.toLowerCase()){
+			searchByNameResults.push(arrayOfPeopleObjects[index]);
 		}
 	}
-	displayPeople(searchByNameResults);
+	mainMenu(searchByNameResults, arrayOfPeopleObjects);
 }
 
-function displayPeople(arrayOfObjectsToBeSearched){
-	alert(arrayOfObjectsToBeSearched.map(function(personObject){
+function mainMenu(foundPersonObject, arrayOfPeopleObjects){
+
+	if(!foundPersonObject){
+	alert("Could not find that individual.");
+	return runApp(arrayOfPeopleObjects);
+	}
+	let displayOption = prompt("Found " + foundPersonObject.map(pluck => pluck.firstName) + " " + foundPersonObject.map(pluck => pluck.lastName) + " . Do you want to know their 'info', 'family', or 'descendants'? Type the option you want or 'restart' or 'quit'");
+	switch(displayOption){
+	case "info":
+	break;
+	case "family":
+	break;
+	case "descendants":
+	getDescendants(foundPersonObject, arrayOfPeopleObjects);
+	break;
+	case "restart":
+	runApp(arrayOfPeopleObjects);
+	break;
+	case "quit":
+	return; // stop execution
+	default:
+	return mainMenu(foundPersonObject, arrayOfPeopleObjects);
+	}
+}
+
+function getDescendants(foundPersonObject, arrayOfPeopleObjects, list, index = 0){
+	for(let index = 0; index < arrayOfPeopleObjects.length; index++){
+		if(foundPersonObject.map(pluck => pluck.id) === function parseParentArray(arrayOfPeopleObjects, index){
+			for(let jindex = 0; jindex < arrayOfPeopleObjects[index].parents.length; jindex++){
+				let foundAParent = arrayOfPeopleObjects[index].parents[jindex];
+				return foundAParent;
+			}
+		}) 
+	}
+}
+
+/*	let i = index || data.findIndex(person => person === person);
+	list = list || [];
+	let desOb = [];
+	let rentId = person[i].id;
+	for (let j = 0; j < people.length && i < person.length; j++){
+		if (people[j].parents == rentId) {
+			list.push(people[j].firstName + " " + people[j].lastName);
+			desOb.push(people[j]);
+		}
+		if (i < person.length && j == people.length){
+			i++;
+		}
+}
+getDescendants(desOb, people, list, i);
+console.log(list)
+}
+*/
+
+function displayPeople(foundPersonObject){
+	alert(foundPersonObject.map(function(personObject){
 		return personObject.firstName + " " + personObject.lastName;
 	}).join("\n"));
 }
@@ -45,9 +106,4 @@ function chars(input){
 	return input;
 }
 
-function getObjectPropertyValueCaseInsensitive(objectToBeSearched, valueToBeSearched){
-	return !!Object.values(objectToBeSearched).find(function(objectValue){
-		return objectValue.toLowerCase() === valueToBeSearched.toLowerCase();
-	})
-}
-
+//	for(let index = 0; index < arrayOfPeopleObjects.length; index++){
---
+++ appAnina.js
@@ -0,0 +1,195 @@
+"use strict";
+/*
+Build all of your functions for displaying and gathering information below (GUI).
+*/
+
+// app is the function called to start the entire application
+function app(people){
+  var searchType = promptFor("Do you know the name of the person you are looking for? Enter 'yes' or 'no'", yesNo).toLowerCase();
+  switch(searchType){
+    case 'yes':
+    searchByName(people);
+    break;
+    case 'no':
+    searchByTraits(people);
+    break;
+    default:
+    alert("Wrong! Please try again, following the instructions dummy. :)");
+    app(people); // restart app
+    break;
+  }
+}
+
+function searchByTraits(people) {
+  let userSearchChoice = prompt("What would you like to search by? 'height', 'weight', 'eye color', 'gender', 'age', 'occupation'.");
+  let filteredPeople;
+
+  switch(userSearchChoice) {
+    case "height":
+      filteredPeople = searchByHeight(people);
+      break;
+    case "weight":
+      filteredPeople = searchByWeight(people);
+      break;
+    // so on and so forth
+    default:
+      alert("You entered an invalid search type! Please try again.");
+      searchByTraits(people);
+      break;
+  }  
+
+  let foundPerson = filteredPeople[0];
+
+  mainMenu(foundPerson, people);
+
+}
+
+function searchByWeight(people) {
+  let userInputWeight = prompt("How much does the person weigh?");
+
+  let newArray = people.filter(function (el) {
+    if(el.weight == userInputWeight) {
+      return true;
+    }
+    // return true if el.height matches userInputHeight
+  });
+
+  return newArray;
+}
+
+// Menu function to call once you find who you are looking for
+function mainMenu(person, people){
+  let i = data.findIndex(person => person === person);
+  /* Here we pass in the entire person object that we found in our search, as well as the entire original dataset of people. We need people in order to find descendants and other information that the user may want. */
+
+  if(!person){
+    alert("Could not find that individual.");
+    return app(people); // restart
+  }
+
+  var displayOption = prompt("Found " + person[i].firstName + " " + person[i].lastName + " . Do you want to know their 'info', 'family', or 'descendants'? Type the option you want or 'restart' or 'quit'");
+
+  switch(displayOption){
+    case "info":
+    displayPerson(person);
+    break;
+    case "family":
+    // TODO: get person's family
+    break;
+    case "descendants":
+    getDescendants(person, people);
+    //displayPeople(list);
+    break;
+    case "restart":
+    app(people); // restart
+    break;
+    case "quit":
+    return; // stop execution
+    default:
+    return mainMenu(person, people); // ask again
+  }
+}
+
+function searchByName(people){
+  var firstN = promptFor("What is the person's first name?", chars);
+  var lastName = promptFor("What is the person's last name?", chars);
+  let results = [];
+    for (var i = 0; i < data.length; i++) {
+      if (people[i].firstName === firstN && people[i].lastName === lastName) {
+        results.push(people[i]);
+      }
+    }
+    mainMenu(results, people);
+    }
+
+  
+  
+  // TODO: find the person using the name they entered
+
+
+/// tested - functioning ///
+function getAge (person) {
+  let i = data.findIndex(person => person === person);
+  let birthDate = new Date(person[i].dob);
+  let today = new Date();
+  let ageYears = (today.getFullYear() - birthDate.getFullYear());
+    if (today.getMonth() < birthDate.getMonth() || today.getMonth() == birthDate.getMonth() && today.getDate() < birthDate.getDate()) {
+      ageYears--;
+    }
+  return ageYears;
+}
+
+// retrieve "by blood" family, USE RECURSION to go through desOb array to find grandkids etc.
+
+// TODO: figure out how to move through the indexes of "person" (i++) 
+//issue with desNames array having 0 in front -- currently not carrying names over in new iteration 
+
+function getDescendants (person, people, list, index = 0) {
+  let i = index || data.findIndex(person => person === person);
+  list = list || [];
+  let desOb = [];
+  let rentId = person[i].id;
+  for (let j = 0; j < people.length && i < person.length; j++) {
+    if (people[j].parents == rentId) {
+      list.push(people[j].firstName + " " + people[j].lastName);
+      desOb.push(people[j]);
+    }
+    if (i < person.length && j == people.length) {
+      i++;
+    }
+}
+getDescendants(desOb, people, list, i);
+console.log(list)
+}
+
+
+function displayDescendants (list){
+  alert("")
+}
+// retrieve immediate family (parents, siblings, current spouse, kids), USE ITERATION//
+function getImFam (person, people) {
+
+}
+
+// alerts a list of people
+function displayPeople(people){
+  let i = data.findIndex(person => person === person);
+  alert(people.map(function(person){
+    return person[i].firstName + " " + person[i].lastName;
+  }).join("\n"));
+}
+
+function displayPerson(person){
+  // print all of the information about a person:
+  // height, weight, age, name, occupation, eye color.
+  let age = getAge(person);
+  let i = data.findIndex(person => person === person);
+  var personInfo = "First Name: " + person[i].firstName + "\n";
+  personInfo += "Last Name: " + person[i].lastName + "\n";
+  personInfo += "DOB: " + person[i].dob + "\n";
+  personInfo += "Age: " + age + "\n";
+  personInfo += "Height: " + person[i].height + "\n";
+  personInfo += "Weight: " + person[i].weight + "\n";
+  personInfo += "Eye Color: " + person[i].eyeColor + "\n";
+  personInfo += "Occupation: " + person[i].occupation + "\n";
+  // TODO: finish getting the rest of the information to display
+  alert(personInfo);
+}
+
+// function that prompts and validates user input
+function promptFor(question, valid){
+  do{
+    var response = prompt(question).trim();
+  } while(!response || !valid(response));
+  return response;
+}
+
+// helper function to pass into promptFor to validate yes/no answers
+function yesNo(input){
+  return input.toLowerCase() == "yes" || input.toLowerCase() == "no";
+}
+
+// helper function to pass in as default promptFor validation
+function chars(input){
+  return input; // default validation only
+}
