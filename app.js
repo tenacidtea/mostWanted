fd93d77e01dfc60c3aecc51590f1b79871a39a1f
Victor D. Verdev <tenacidtea@gmail.com>
2018-04-22 12:18:45 -0500

    removed merge test verbiage

--- app.js
+++ app.js
@@ -211,16 +211,13 @@ function getAge (person) {
   //   }
   // return ageYears;
 }
+
 // retrieve "by blood" family, USE RECURSION to go through desOb array to find grandkids etc.
 
 // TODO: figure out how to move through the indexes of "person" (i++) 
 //issue with desNames array having 0 in front -- currently not carrying names over in new iteration
 
-/// Ths is a second line added in sandbox
-/// This line added in sandbox.  Also going to remove empty line below function closing brace.
 
-
-/// testing --patch merge with sandbox.  Also going to remove empty line under closing brace for getAge function.
 function getDescendants(person, people, descendants = []){
     let personId = person.map(pluck => pluck.id);
     for(let i = 0; i < people.length; i++){
@@ -230,6 +227,7 @@ function getDescendants(person, people, descendants = []){
     }
     return getDescendants(person, people, descendants);
 }
+
 /*
 function getDescendants (person, people, list, index = 0) {
   let i = index || data.findIndex(person => person === person);
